d4d2cb1b83f6cdb058d34676adbfad3dfb9cdf6e
karthik <karthik.jk11@gmail.com>
2022-10-05 14:51:41 +0530

    fix root dir

--- hack/preview.sh
+++ hack/preview.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/..
+ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/
 
 if [ -f $ROOT/hack/preview.env ]; then
     source $ROOT/hack/preview.env
